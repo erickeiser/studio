053d2f62df496e8cc25f86270516d086a404703e
Eric Keiser <Eric.Keiser1@gmail.com>
2025-11-14 03:20:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3072336315-97d00",
+  "appId": "1:839118751812:web:c6cb6d08cf10b9f0989411",
+  "apiKey": "AIzaSyCiefeyY3oAoXNTMaD-b0Saej2j3QqyI-8",
+  "authDomain": "studio-3072336315-97d00.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "839118751812"
+};
